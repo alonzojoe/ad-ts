5967e9dd0ae30fc7b1a86e6d90884c3d1d1cfdff
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-04 16:51:54 +0800

    refactor union type, additional properties e.g padding type number[]

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 
 // const ExampleComponent: React.FC = () => {}; //works only on arrow functions and also add children prop-type automatically
-  
+
+type Color = "red" | "blue" | "green" | "yellow" | "purple";
+
 type ButtonProps = {
-  backgroundColor: "red" | "blue" | "green"; //union type
+  backgroundColor: Color; //union type
+  textColor?: Color;
   pillShape?: boolean;
+  padding: number[];
 };
 
-const Button = ({ backgroundColor, pillShape }: ButtonProps) => {
+const Button = ({
+  backgroundColor,
+  textColor,
+  pillShape,
+  padding,
+}: ButtonProps) => {
   return (
     <button className="bg-blue-500 cursor-pointer text-white rounded px-4 py-2">
-      Click Me  
+      Click Me
     </button>
   );
 };

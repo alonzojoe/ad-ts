0833651a2dc950cc97fb0aa15f89f8604ff8f0d2
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-04 16:48:10 +0800

    union types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 
 // const ExampleComponent: React.FC = () => {}; //works only on arrow functions and also add children prop-type automatically
-
+  
 type ButtonProps = {
-  backgroundColor: string;
+  backgroundColor: "red" | "blue" | "green"; //union type
+  pillShape?: boolean;
 };
 
-const Button = ({ backgroundColor = "bg-blue-500" }: ButtonProps) => {
+const Button = ({ backgroundColor, pillShape }: ButtonProps) => {
   return (
     <button className="bg-blue-500 cursor-pointer text-white rounded px-4 py-2">
-      Click Me
+      Click Me  
     </button>
   );
 };

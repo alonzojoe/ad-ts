83de9ac9f14e7b51d8cf9522e6c8827fe26a7a3e
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-04 16:15:25 +0800

    create new reusable button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className="bg-blue-500 cursor-pointer text-white rounded px-4 py-2">
+      Click Me
+    </button>
+  );
+};
+
+export default Button;

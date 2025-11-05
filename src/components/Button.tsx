fd9b90f8f237fb219e6bc007e2c018829099058d
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-05 15:31:33 +0800

    update vite config add watch usePolling under server.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -19,7 +19,7 @@ const Button = ({
 }: ButtonProps) => {
   return (
     <button className="bg-blue-500 cursor-pointer text-white rounded px-4 py-2">
-      Click Me
+      Click Me!
     </button>
   );
 };

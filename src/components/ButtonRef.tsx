3ce09e0aca84bdeda7c3598d73a69cddc6e31e2e
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-05 22:30:40 +0800

    as keyword, Omit Utility Type as const

--- src/components/ButtonRef.tsx
+++ src/components/ButtonRef.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import { useEffect } from "react";
+
+const buttonTextOptions = [
+  "Click Me",
+  "Click Me Again",
+  "Click Me One More Time",
+] as const;
+
+type User = {
+  sessionToken: string;
+  name: string;
+};
+
+type Guest = Omit<User, "sessionToken">;
+
+type ButtonColor = "red" | "blue" | "green";
 
 const ButtonRef = () => {
+  useEffect(() => {
+    const buttonColor = localStorage.getItem("buttonColor") as ButtonColor;
+  }, []);
+
   return (
     <button className="bg-green-500 cursor-pointer text-white rounded px-4 py-2 my-10">
-      Button Ref
+      {buttonTextOptions.map((option) => {
+        return option;
+      })}
     </button>
   );
 };

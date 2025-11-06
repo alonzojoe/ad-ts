283915fad16eada83c8ed1b2e311437d22cf6305
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-06 11:45:12 +0800

    todo form validation implement ref in input elelement with type-safe

--- src/components/Todos/TodoForm.tsx
+++ src/components/Todos/TodoForm.tsx
@@ -1,28 +1,36 @@
-import { type FormEvent } from "react";
+import { type FormEvent, useRef } from "react";
 import { useContext } from "react";
 import TodoContext from "../../context/todo/todo-context";
 import type { ITodoContext } from "../../types/@types.todo";
 
 const TodoForm = () => {
+  const titleRef = useRef<HTMLInputElement>(null);
   const { addTodo } = useContext(TodoContext) as ITodoContext;
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    const inputEl = titleRef.current;
+    if (!inputEl) return;
+
+    const todoTitle = inputEl.value.trim();
+    if (todoTitle === "") return;
+
     const newTodo = {
       id: Date.now(),
-      title: "",
+      title: todoTitle,
       completed: false,
     };
 
     addTodo(newTodo);
+    inputEl.value = "";
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="">Title:</label>
-        <input type="text" />
+        <input ref={titleRef} type="text" />
         <button>Add</button>
       </div>
     </form>

67f622362a869894042057918659d68e92211a02
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-07 16:39:39 +0800

    Refactor Settings make it Generic Component

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -10,18 +10,41 @@ const Settings = () => {
   return (
     <Container>
       <h1 className="text-2xl font-bold">Settings</h1>
-      <ul className="list-disc space-y-2 text-lg mt-5">
-        {SETTINGS.map((item, index) => (
-          <li
-            className={`cursor-pointer ${active === item ? "font-bold" : ""}`}
-            key={`item-${index}`}
-            onClick={() => setActive(item)}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
+      <Options
+        selections={SETTINGS}
+        selected={active}
+        onSelect={(theme) => setActive(theme)}
+      />
+      <p className="my-5 text-2xl">
+        Selected Control: <span className="font-bold">{active}</span>
+      </p>
     </Container>
+  );
+};
+
+type OptionsProps<T> = {
+  selections: readonly T[];
+  selected: T;
+  onSelect: (theme: T) => void;
+};
+
+const Options = <T extends string | number>({
+  selections,
+  selected,
+  onSelect,
+}: OptionsProps<T>) => {
+  return (
+    <ul className="list-disc space-y-2 text-xl mt-5">
+      {selections.map((item, index) => (
+        <li
+          className={`cursor-pointer ${selected === item ? "font-bold" : ""}`}
+          key={`item-${index}`}
+          onClick={() => onSelect(item)}
+        >
+          {item}
+        </li>
+      ))}
+    </ul>
   );
 };
 

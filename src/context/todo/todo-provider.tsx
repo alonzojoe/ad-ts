3afcffe6c133ac1820df6d82df4b725eee2b2198
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-07 14:26:29 +0800

    refactor todo-provider replace types with the created todos.type.ts file

--- src/context/todo/todo-provider.tsx
+++ src/context/todo/todo-provider.tsx
@@ -1,36 +1,26 @@
 import React, { useState, useReducer } from "react";
 import type { ITodo } from "../../types/@types.todo";
 import TodoContext from "./todo-context";
+import { type Action, ACTIONS } from "./types/todo.types";
 
 type TodoProviderProps = {
   children: React.ReactNode;
 };
 
-export const ACTIONS = {
-  ADD_TODO: "add-todo",
-  REMOVE_TODO: "remove-todo",
-  TOGGLE_TODO: "toggle-todo",
-} as const;
-
-type Action =
-  | { type: typeof ACTIONS.ADD_TODO; payload: ITodo }
-  | { type: typeof ACTIONS.REMOVE_TODO; payload: number }
-  | { type: typeof ACTIONS.TOGGLE_TODO; payload: number };
-
 const todoReducer = (todos: ITodo[], action: Action): ITodo[] => {
   switch (action.type) {
     case ACTIONS.ADD_TODO: {
-      const newTodo = action.payload;
+      const newTodo = action.payload.todo;
       return [newTodo, ...todos];
     }
 
     case ACTIONS.REMOVE_TODO: {
-      const id = action.payload;
+      const id = action.payload.id;
       return todos.filter((todo) => todo.id !== id);
     }
 
     case ACTIONS.TOGGLE_TODO: {
-      const id = action.payload;
+      const id = action.payload.id;
 
       return todos.map((todo) => {
         if (todo.id === id) {
@@ -54,16 +44,16 @@ const TodoProvider = ({ children }: TodoProviderProps) => {
 
   const addTodo = (todo: ITodo) => {
     // setTodos((prev) => [todo, ...prev]);
-    dispatchTodo({ type: ACTIONS.ADD_TODO, payload: todo });
+    dispatchTodo({ type: ACTIONS.ADD_TODO, payload: { todo } });
   };
 
   const deleteTodo = (id: number) => {
     // setTodos((todos) => todos.filter((todo) => todo.id !== id));
-    dispatchTodo({ type: ACTIONS.REMOVE_TODO, payload: id });
+    dispatchTodo({ type: ACTIONS.REMOVE_TODO, payload: { id } });
   };
 
   const toggleTodo = (id: number) => {
-    dispatchTodo({ type: ACTIONS.TOGGLE_TODO, payload: id });
+    dispatchTodo({ type: ACTIONS.TOGGLE_TODO, payload: { id } });
     // setTodos((todos) =>
     //   todos.map((todo) => {
     //     if (todo.id === id) {

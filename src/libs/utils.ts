d0382e5be61a38a2d9a717929489921581c766b5
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-09 22:43:51 +0800

    the ReturnType and Awaited Utility Types

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -30,6 +30,7 @@ createArrayPair(true, 1)
 
 
 //useful ts
+//1. typeof / keyof
 const person = 'joe'
 type Person = typeof person
 
@@ -40,5 +41,43 @@ const obj = {
 }
 
 type Obj = keyof typeof obj
+
+//2 Fn Return Type
+
+const sumNum = (a: number, b: number) => {
+    return a + b
+}
+
+type FnReturn = ReturnType<typeof sumNum>
+
+type Todo = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+const fetchData = async <APIResponse>(): Promise<APIResponse> => {
+    try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+
+        if (!res.ok) {
+            throw new Error(`An error occured while fetching the data: ${res.status}`)
+        }
+
+        const data = await res.json()
+
+        return data as APIResponse
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log(error.message)
+        }
+        throw error;
+    }
+}
+
+
+type TodoData = Awaited<ReturnType<typeof fetchData<Todo>>>
+
 
 export default ""

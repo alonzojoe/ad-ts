9b904b8466f6dab173a818b7f307c4b0ee1348f0
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-12 09:45:47 +0800

    more of generic types accepts multiple arg

--- src/libs/generics.ts
+++ src/libs/generics.ts
@@ -7,3 +7,21 @@ const identity = <T>(value: T) => {
 identity(42)
 identity("hello")
 identity({ name: "A" })
+
+
+const getFirstElement = <T>(arr: T[]) => {
+    if (arr.length === 0) return undefined;
+
+    return arr[0]
+}
+
+getFirstElement([1, 2, 3]);
+getFirstElement(["a", "b", "c"]);
+getFirstElement([]);
+
+const mergeObjects = <T, U>(obj1: T, obj2: U) => {
+    return { ...obj1, ...obj2 }
+}
+
+mergeObjects({ name: "Alice" }, { age: 25 });
+mergeObjects({ id: 1 }, { isAdmin: true });

4e5ba07b11862162268f1457270ed6f19f2bddab
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-14 11:01:06 +0800

    implement GenericList in Home component supplied required props: items, getRow and getKey

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import Container from "../components/Shared/Container";
-
+import GenericList from "../components/Lists/GenericList";
 const Home = () => {
   return (
     <Container>
       <h1 className="text-2xl font-bold">Home Page</h1>
+      <GenericList
+        items={[
+          { id: 1, title: "The Road not taken", isRead: true },
+          { id: 2, title: "Harry potter" },
+        ]}
+        getKey={(poem) => poem.id}
+        getRow={(poem) =>
+          `The Book title is '${poem.title}', Read: ${poem.isRead || "unkown"}`
+        }
+      />
     </Container>
   );
 };

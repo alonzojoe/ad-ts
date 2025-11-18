df9cb626a0c7de75ca6fcbd1c3a2eae4d71aa326
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-18 16:41:00 +0800

    cleanup Hooks Page remove unused codes and imports.

--- src/pages/Hooks.tsx
+++ src/pages/Hooks.tsx
@@ -1,38 +1,7 @@
-import { useState } from "react";
-import usePrevious from "../hooks/usePrevious";
 import Container from "../components/Shared/Container";
 import UsePrevious from "../features/hooks/UsePrevious";
 
-const getRandomColor = () => {
-  const colors = ["red", "blue", "green", "pink", "purple"] as const;
-  return colors[Math.floor(Math.random() * colors.length)];
-};
-
-const mappedColors = {
-  red: "bg-red-600",
-  blue: "bg-blue-600",
-  green: "bg-green-600",
-  pink: "bg-pink-600",
-  purple: "bg-purple-600",
-} as const;
-
 const Hooks = () => {
-  const [color, setColor] = useState(getRandomColor());
-  const previousColor = usePrevious(color);
-
-  const handleClick = () => {
-    let newColor = getRandomColor();
-
-    while (color === newColor) {
-      newColor = getRandomColor();
-    }
-
-    setColor(newColor);
-  };
-
-  console.log(`previous: ${previousColor}`);
-  console.log(`current: ${color}`);
-
   return (
     <Container>
       <h1 className="">Hooks</h1>

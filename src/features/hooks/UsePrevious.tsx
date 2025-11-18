aeb63b5a92c37d04dfb0a778b8d325e0eee87b76
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-18 16:40:32 +0800

    crea new separate component for usePrevious move all codes jsx and logic,state in new fle

---
+++ src/features/hooks/UsePrevious.tsx
@@ -0,0 +1,61 @@
+import usePrevious from "../../hooks/usePrevious";
+import { useState } from "react";
+
+const getRandomColor = () => {
+  const colors = ["red", "blue", "green", "pink", "purple"] as const;
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const mappedColors = {
+  red: "bg-red-600",
+  blue: "bg-blue-600",
+  green: "bg-green-600",
+  pink: "bg-pink-600",
+  purple: "bg-purple-600",
+} as const;
+
+const UsePrevious = () => {
+  const [color, setColor] = useState(getRandomColor());
+  const previousColor = usePrevious(color);
+
+  const handleClick = () => {
+    let newColor = getRandomColor();
+
+    while (color === newColor) {
+      newColor = getRandomColor();
+    }
+
+    setColor(newColor);
+  };
+
+  console.log(`previous: ${previousColor}`);
+  console.log(`current: ${color}`);
+
+  return (
+    <>
+      <div className="flex flex-col space-y-2 items-center justify-center p-3">
+        <h1 className="text-2xl font-bold">usePrevious</h1>
+        <button
+          className="underline text-blue-600 cursor-pointer"
+          onClick={handleClick}
+        >
+          next
+        </button>
+      </div>
+
+      <div className="w-100 flex justify-center items-center gap-3 p-5 bg-gray-100 shadow-md rounded">
+        <div className="w-100/3 flex flex-col items-center justify-center">
+          <div className={`rounded bg-${previousColor}-600 w-full p-10 mt-3`} />
+          <span className="text-base mt-2">Previous</span>
+        </div>
+
+        <div className="w-100/3 flex flex-col items-center justify-center">
+          <div className={`rounded bg-${color}-600 w-full p-10 mt-3`} />
+          <span className="text-base mt-2">Current</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UsePrevious;
--- src/pages/Hooks.tsx
+++ src/pages/Hooks.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import usePrevious from "../hooks/usePrevious";
 import Container from "../components/Shared/Container";
+import UsePrevious from "../features/hooks/UsePrevious";
 
 const getRandomColor = () => {
   const colors = ["red", "blue", "green", "pink", "purple"] as const;
@@ -36,27 +37,7 @@ const Hooks = () => {
     <Container>
       <h1 className="">Hooks</h1>
 
-      <div className="flex flex-col space-y-2 items-center justify-center p-3">
-        <h1 className="text-2xl font-bold">usePrevious</h1>
-        <button
-          className="underline text-blue-600 cursor-pointer"
-          onClick={handleClick}
-        >
-          next
-        </button>
-      </div>
-
-      <div className="w-100 flex justify-center items-center gap-3 p-5 bg-gray-100 shadow-md rounded">
-        <div className="w-100/3 flex flex-col items-center justify-center">
-          <div className={`rounded bg-${previousColor}-600 w-full p-10 mt-3`} />
-          <span className="text-base mt-2">Previous</span>
-        </div>
-
-        <div className="w-100/3 flex flex-col items-center justify-center">
-          <div className={`rounded bg-${color}-600 w-full p-10 mt-3`} />
-          <span className="text-base mt-2">Current</span>
-        </div>
-      </div>
+      <UsePrevious />
     </Container>
   );
 };

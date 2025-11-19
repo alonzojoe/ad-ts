60ca74ef2f647336b5f2cf3fced390f748958681
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-19 16:35:17 +0800

    create HOOS sample array and HookItem replace hard coded element inside the GridContainer.

--- src/pages/Hooks.tsx
+++ src/pages/Hooks.tsx
@@ -2,6 +2,13 @@ import Container from "../components/Shared/Container";
 import UsePrevious from "../features/hooks/UsePrevious";
 import GridContainer from "../components/Shared/GridContainer";
 
+const HOOKS = [
+  { id: 1, title: "useDebounce", description: "Description here" },
+  { id: 2, title: "useDebounce", description: "Description here" },
+  { id: 3, title: "useDebounce", description: "Description here" },
+  { id: 4, title: "useDebounce", description: "Description here" },
+];
+
 const Hooks = () => {
   return (
     <Container>
@@ -9,13 +16,26 @@ const Hooks = () => {
 
       <UsePrevious />
 
-      <GridContainer className="w-full bg-red-500" cols={4}>
-        <div>A</div>
-        <div>B</div>
-        <div>C</div>
-        <div>D</div>
+      <GridContainer className="w-full gap-5" cols={4}>
+        {HOOKS.map(({ title, description }) => (
+          <HookItem title={title} description={description} />
+        ))}
       </GridContainer>
     </Container>
+  );
+};
+
+type HookItemProps = {
+  title: string;
+  description: string;
+};
+
+const HookItem = ({ title, description }: HookItemProps) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
   );
 };
 

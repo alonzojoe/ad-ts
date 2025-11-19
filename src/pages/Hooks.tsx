3a7198e38942e2449a7ecb593359beb43c49ff66
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-19 15:49:23 +0800

    create new grid container in hooks page component

--- src/pages/Hooks.tsx
+++ src/pages/Hooks.tsx
@@ -7,6 +7,12 @@ const Hooks = () => {
       <h1 className="">Hooks</h1>
 
       <UsePrevious />
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-3 xl:grid-cols-4 w-full bg-red-500">
+        <div>A</div>
+        <div>B</div>
+        <div>C</div>
+        <div>D</div>
+      </div>
     </Container>
   );
 };

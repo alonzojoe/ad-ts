af171ddbd00a0b8f9cc8016cd84daabe4ab3fa26
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-23 22:13:37 +0800

    update Hooks page implement the hooklist, change the prop of HookItem to title, following the shape of hook-lists item

--- src/pages/Hooks.tsx
+++ src/pages/Hooks.tsx
@@ -1,18 +1,7 @@
 import Container from "../components/Shared/Container";
 import UsePrevious from "../features/hooks/UsePrevious";
 import GridContainer from "../components/Shared/GridContainer";
-
-const HOOKS = [
-  {
-    id: 1,
-    title: "useDebounce",
-    description:
-      "Delay the execution of function or state update with useDebounce",
-  },
-  { id: 2, title: "useDebounce", description: "Description here" },
-  { id: 3, title: "useDebounce", description: "Description here" },
-  { id: 4, title: "useDebounce", description: "Description here" },
-];
+import { HOOKS } from "../constants/hook-lists";
 
 const Hooks = () => {
   return (
@@ -22,8 +11,8 @@ const Hooks = () => {
       <UsePrevious />
 
       <GridContainer className="mx-auto w-full gap-5" cols={4}>
-        {HOOKS.map(({ id, title, description }) => (
-          <HookItem key={id} title={title} description={description} />
+        {HOOKS.map(({ id, name, description }) => (
+          <HookItem key={id} name={name} description={description} />
         ))}
       </GridContainer>
     </Container>
@@ -31,14 +20,14 @@ const Hooks = () => {
 };
 
 type HookItemProps = {
-  title: string;
+  name: string;
   description: string;
 };
 
-const HookItem = ({ title, description }: HookItemProps) => {
+const HookItem = ({ name, description }: HookItemProps) => {
   return (
     <div className="p-5 bg-gray-100 rounded-xl shadow-md cursor-pointer transition-all duration-200 hover:-translate-y-1 hover:shadow-lg">
-      <h1 className="font-bold text-2xl text-sky-500 leading-tight">{title}</h1>
+      <h1 className="font-bold text-2xl text-sky-500 leading-tight">{name}</h1>
       <p className="text-lg mt-3">{description}</p>
     </div>
   );

2c54f2571bc07e4b5b7e55d2f98d17892e1620ef
alonzojoe <alonzojoenell2k15@gmail.com>
2025-12-09 14:49:01 +0800

    add 2 new controls reload page and clear storage

--- src/features/hooks/UseLocalStorage.tsx
+++ src/features/hooks/UseLocalStorage.tsx
@@ -1,7 +1,22 @@
 const UseLocalStorage = () => {
+  const reloadPage = () => window.location.reload();
+
+  const clearStorage = () => {
+    window.localStorage.clear();
+    reloadPage();
+  };
+
   return (
     <div className="flex flex-col space-y-2 items-center justify-center p-3">
       <h1 className="text-2xl font-bold">useLocalStorage</h1>
+      <div className="flex items-center justify-between text-blue-600 underline gap-5">
+        <span onClick={reloadPage} className="cursor-pointer">
+          Reload Page
+        </span>
+        <span onClick={clearStorage} className="cursor-pointer">
+          Clear Storage
+        </span>
+      </div>
     </div>
   );
 };

62873408b8757b98aae626ac1aee8d9738126098
alonzojoe <alonzojoenell2k15@gmail.com>
2025-12-11 11:22:10 +0800

    create ios switch button and implement the toggle logic

--- src/features/hooks/UseToggle.tsx
+++ src/features/hooks/UseToggle.tsx
@@ -1,7 +1,6 @@
 import { type ComponentProps } from "react";
 import useToggle from "../../hooks/useToggle";
 
-
 const UseToggle = () => {
   const [value, toggleValue] = useToggle(false);
 
@@ -21,8 +20,19 @@ const UseToggle = () => {
         />
         <ToggleItem onClick={() => toggleValue()} label="Toggle" />
       </div>
-      <div className="mt-5 w-100 flex flex-col justify-center items-center gap-3 p-5 bg-gray-100 shadow-md rounded">
-        {JSON.stringify(value)}
+      <div className="mt-5 w-100 flex flex-col justify-center items-center gap-3 p-5 bg-white shadow-md rounded">
+        <label className="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            checked={value}
+            onChange={(e) => toggleValue(e.target.checked)}
+            className="sr-only peer"
+          />
+          <div className="relative w-9 bg-gray-300 border-2 border-gray-300 h-5 bg-neutral-gray-300 peer-focus:outline-none peer-focus:ring-0 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-0 after:start-px after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-400"></div>
+          <span className="ms-3 text-sm font-medium text-heading select-none">
+            {value ? "On" : "Off"}
+          </span>
+        </label>
       </div>
     </div>
   );

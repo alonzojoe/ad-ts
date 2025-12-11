f810d33fca076dc38cbcc9da9a9819c516359853
alonzojoe <alonzojoenell2k15@gmail.com>
2025-12-11 10:56:19 +0800

    remove extra state (over-enginered*)

--- src/features/hooks/UseToggle.tsx
+++ src/features/hooks/UseToggle.tsx
@@ -1,38 +1,25 @@
 import { type ComponentProps } from "react";
-import { useState } from "react";
 import useToggle from "../../hooks/useToggle";
 
-type Status = boolean;
 
 const UseToggle = () => {
-  const [status, setStatus] = useState<Status>(false);
   const [value, toggleValue] = useToggle(false);
-
-  const handleChange = (val: Status) => {
-    setStatus(val);
-    toggleValue(val);
-  };
-
-  const handleToggle = () => {
-    setStatus((prev) => !prev);
-    toggleValue();
-  };
 
   return (
     <div className="flex flex-col space-y-2 items-center justify-center p-3">
       <h1 className="text-2xl font-bold">useToggle</h1>
       <div className="flex items-center mt-5 justify-between gap-5">
         <ToggleItem
-          onClick={() => handleChange(true)}
-          disabled={status === true}
+          onClick={() => toggleValue(true)}
+          disabled={value === true}
           label="Turn On"
         />
         <ToggleItem
-          disabled={status === false}
-          onClick={() => handleChange(false)}
+          disabled={value === false}
+          onClick={() => toggleValue(false)}
           label="Turn Off"
         />
-        <ToggleItem onClick={handleToggle} label="Toggle" />
+        <ToggleItem onClick={() => toggleValue()} label="Toggle" />
       </div>
       <div className="mt-5 w-100 flex flex-col justify-center items-center gap-3 p-5 bg-gray-100 shadow-md rounded">
         {JSON.stringify(value)}
